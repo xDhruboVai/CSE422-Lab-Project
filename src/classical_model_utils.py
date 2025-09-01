553761f9cb6d592d2f7eb969c522c893735fea4a
Dihan Islam Dhrubo <dihanislamdhrubo@SMs-MacBook-Pro.local>
2025-09-01 21:03:00 +0600

    Add dataset description

--- src/classical_model_utils.py
+++ src/classical_model_utils.py
@@ -2,14 +2,12 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report
-import numpy as np
+from sklearn.metrics import accuracy_score
 import pandas as pd
 
 import sys
 import os
 sys.path.append(os.path.abspath('/Users/dihanislamdhrubo/Downloads/CSE422-Lab-Project-main'))
-from src import evaluations
 
 X_train_scaled = pd.read_csv("/Users/dihanislamdhrubo/Downloads/CSE422-Lab-Project-main/data/X_train_scaled.csv")
 X_test_scaled = pd.read_csv("/Users/dihanislamdhrubo/Downloads/CSE422-Lab-Project-main/data/X_test_scaled.csv")
@@ -19,7 +17,6 @@ y_train = pd.read_csv("/Users/dihanislamdhrubo/Downloads/CSE422-Lab-Project-main
 y_test = pd.read_csv("/Users/dihanislamdhrubo/Downloads/CSE422-Lab-Project-main/data/y_test.csv")
 y_train = y_train.values.ravel()
 y_test = y_test.values.ravel()
-
 
 def knn_predict():
     boss_y = None
@@ -36,7 +33,7 @@ def knn_predict():
             boss_y = y_pred
             max_accuracy = accuracy
             boss_k = k
-    
+
     print("Best value of K is", boss_k)
     return boss_y, max_accuracy
 
@@ -67,9 +64,6 @@ def log_predict():
     )
 
     log_reg.fit(X_train_scaled, y_train)
-
     y_pred = log_reg.predict(X_test_scaled)
-
     accuracy = accuracy_score(y_pred, y_test)
-
     return y_pred, accuracy
