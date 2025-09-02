dcd382d7b6043512113b0ac6a48062623de8b1d9
Dihan Islam Dhrubo <dihanislamdhrubo@SMs-MacBook-Pro.local>
2025-09-02 17:42:54 +0600

    Jani na bhai ki kortesi

--- src/classical_model_utils.py
+++ src/classical_model_utils.py
@@ -4,6 +4,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import pandas as pd
+import numpy as np
 
 import sys
 import os
@@ -19,6 +20,7 @@ y_train = y_train.values.ravel()
 y_test = y_test.values.ravel()
 
 def knn_predict():
+    boss_knn = None
     boss_y = None
     boss_k = None
     max_accuracy = float("-inf")
@@ -33,9 +35,10 @@ def knn_predict():
             boss_y = y_pred
             max_accuracy = accuracy
             boss_k = k
+            boss_knn = knn
 
     print("Best value of K is", boss_k)
-    return boss_y, max_accuracy
+    return boss_knn, boss_y, max_accuracy
 
 def nb_predict():
     nb = GaussianNB()
@@ -43,7 +46,7 @@ def nb_predict():
     nb.fit(X_train_scaled, y_train)
     y_pred = nb.predict(X_test_scaled)
     accuracy = accuracy_score(y_test, y_pred)
-    return y_pred, accuracy
+    return nb, y_pred, accuracy
 
 def dt_predict():
     dt = DecisionTreeClassifier(
@@ -54,7 +57,49 @@ def dt_predict():
     dt.fit(X_train, y_train)
     y_pred = dt.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
-    return y_pred, accuracy
+    return dt, y_pred, accuracy
+
+'''def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+
+class Logistic_Regression_GD:
+    def __init__(self, lr = 0.01, epochs = 1000):
+        self.lr = lr
+        self.epochs = epochs
+
+    def fit(self, X, y):
+        m, n = X.shape
+        self.w = np.zeros(n) #w
+        self.b = 0           #beta
+        self.losses = []
+
+        for _ in range(self.epochs):
+            linear_model = np.dot(X, self.w) + self.b
+            y_pred = sigmoid(linear_model)
+
+            dw = (1/m) * np.dot(X.T, (y_pred - y))
+            db = (1/m) * np.sum(y_pred - y)
+
+            self.w -= self.lr * dw
+            self.b -= self.lr * db
+
+            loss = -(1 / m) * np.sum(y * np.log(y_pred + 1e-10) + (1-y) * np.log(1 - y_pred + 1e-10))
+            self.losses.append(loss)
+
+    def predict(self, X):
+        linear_model = np.dot(X, self.w) + self.b
+        y_pred = sigmoid(linear_model)
+        return np.where(y_pred >= 0.5, 1, 0)
+
+def log_predict():
+    log_reg = Logistic_Regression_GD()
+
+    log_reg.fit(X_train_scaled, y_train)
+    y_pred = log_reg.predict(X_test_scaled)
+    accuracy = accuracy_score(y_pred, y_test)
+    return y_pred, accuracy'''
+
 
 def log_predict():
     log_reg = LogisticRegression(
@@ -66,4 +111,4 @@ def log_predict():
     log_reg.fit(X_train_scaled, y_train)
     y_pred = log_reg.predict(X_test_scaled)
     accuracy = accuracy_score(y_pred, y_test)
-    return y_pred, accuracy
+    return log_reg, y_pred, accuracy
